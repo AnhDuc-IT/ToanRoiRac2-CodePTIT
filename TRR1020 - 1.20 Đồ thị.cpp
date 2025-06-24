7aeae012c54da3ebfc87ab09f3a2b763e30ddf92
Mai Anh Đức <duckuro.ptit@gmail.com>
2025-06-24 21:46:37 +0700

    Create TRR1020 - 1.20 Đồ thị.cpp

---
+++ TRR1020 - 1.20 Đồ thị.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define ii pair <int, int>
+#define iii pair<int, pair <int,int>>
+#define MOD 1e9 + 7;
+void File(){
+    freopen("DT.INP", "r", stdin);
+    freopen("DT.OUT", "w", stdout);
+}
+set <ii> adj;
+int n, t, m, in_de[1005], out_de[1005];
+int a[1005][1005];
+void nhap(){
+    cin >> t >> n;
+    int dem = 1;
+    for(int i = 1; i <= n; i++){
+        int k; cin >> k;
+        for(int j = 1; j <= k; j++){
+            int x; cin >> x;
+            out_de[i]++;
+            in_de[x]++;
+            a[i][dem] = 1;
+            a[x][dem] = -1;
+            ++m;
+            ++dem;
+        }
+    }
+}
+void solve(){
+    if(t == 1){
+        for(int i = 1; i <= n; i++){
+            cout << in_de[i] << " " << out_de[i] << endl;
+        }
+    }
+    else{
+        cout << n << ' ' << m << endl;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+   File();
+   nhap();
+   solve();
+}
