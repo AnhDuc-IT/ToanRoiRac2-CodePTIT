b15b3ab95dcd9aea71d06ecb5445446e8d43dd12
Mai Anh Đức <duckuro.ptit@gmail.com>
2025-06-24 21:43:05 +0700

    Create TRR1011 - 1.11 Đồ thị.cpp

---
+++ TRR1011 - 1.11 Đồ thị.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define ii pair <int, int>
+#define iii pair<int, pair <int,int>>
+#define MOD 1e9 + 7;
+void File(){
+    freopen("DT.INP", "r", stdin);
+    freopen("DT.OUT", "w", stdout);
+}
+vector <vector<int>> a(1000 + 1, vector <int> (1000 + 1, 10000));
+int n, m, t, de[1005];
+void nhap(){
+    cin >> t >> n >> m;
+    for(int i = 1; i <= m; i++){
+        int u, v, w; cin >> u >> v >> w;
+        de[u]++;
+        de[v]++;
+        a[u][v] = w;
+        a[v][u] = w;
+    }
+}
+void solve(){
+    if(t == 1){
+        for(int i = 1; i <= n; i++){
+            cout << de[i] << " ";
+        }
+    }
+    else{
+        cout << n << endl;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == j) cout << 0 << " ";
+                else cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    File();
+    nhap();
+    solve();
+}
